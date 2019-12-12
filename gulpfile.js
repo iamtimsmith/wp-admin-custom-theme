a0c01e1b21397cec3e679b8f5742c72587581a28
Tim Smith <timsmith@Tims-MacBook-Pro.local>
2019-12-11 21:54:53 -0600

    Updated styles to be more normal, subtle colors.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const { task, series, src, dest, watch } = require('gulp');
 const sass = require('gulp-sass');
 const cssnano = require('gulp-cssnano');
 const rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
 
 task('styles', function (done) {
   src('scss/**/*.scss')
@@ -11,10 +12,30 @@ task('styles', function (done) {
 });
 
 task('minify', function (done) {
-  src('css/*.css')
+  src('css/admin.css')
+    .pipe(autoprefixer({
+      "overrideBrowserslist": [
+        "> 1%",
+        "last 2 versions",
+        "ie >= 11"
+      ]
+    }))
     .pipe(cssnano())
     .pipe(rename({ suffix: '.min' }))
     .pipe(dest('css'));
+
+  src('css/frontend.css')
+    .pipe(autoprefixer({
+      "overrideBrowserslist": [
+        "> 1%",
+        "last 2 versions",
+        "ie >= 11"
+      ]
+    }))
+    .pipe(cssnano())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(dest('css'));
+
   done();
 });
 
