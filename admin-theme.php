6d46834deaf5626ff4f372cad40dbb33b07d8a33
Tim Smith <tgsmith489@gmail.com>
2022-11-15 09:43:00 -0600

    Made font thicker. Removed gap in gutenberg sidebar.

--- admin-theme.php
+++ admin-theme.php
@@ -8,14 +8,14 @@
 
 // Update CSS within in Admin
 function admin_style() {
-  wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:300&display=swap');
+  wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:400&display=swap');
   wp_enqueue_style('admin-styles', plugins_url('css/admin.min.css', __FILE__));
   wp_enqueue_script('admin-scripts', plugins_url('js/admin.js', __FILE__), array('jquery'), '1.0.0', true);
 }
 add_action('admin_init', 'admin_style');
 
 function frontend_style() {
-  wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:300&display=swap');
+  wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:400&display=swap');
   wp_enqueue_style('frontend-styles', plugins_url('css/frontend.min.css', __FILE__));
 }
 add_action('wp_enqueue_scripts', 'frontend_style');
